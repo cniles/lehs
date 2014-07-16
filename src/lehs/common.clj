358b8744ee50ec580d0de4c625ff605ca195d004
Craig Niles <niles.c@gmail.com>
2014-07-16 17:21:05 -0400

    Refactored some code into more generic functions
    Refactored stream-seq out of read head, and key/value extraction from
    message body and uri query into get-key-value and map-funcs.

--- src/lehs/common.clj
+++ src/lehs/common.clj
@@ -7,4 +7,18 @@
         :else (recur (conj h (first s)) (drop 1 s))))))
 
 (defn stream-seq [s]
+  "Returns a lazy sequence of the items read off of a stream"
       (repeatedly #(char (.read s))))
+
+(defn map-funcs [fs c]
+  "Returns a sequence consisting of applying first item in fs to the
+  first item in c, and the second item in fs to the second item in c.
+  That is, fs needs to be a sequence of functions taking
+  one-argument (i.e. [f1, f2, f3, ..., fn]).  The returned result is the
+  sequence [(f1 c1) (f2 c2) (f3 c3) ... (fn cn)]."
+  (map #(% %2) fs c))
+
+(defn get-key-value [s d]
+  "Splits a string at its delimeter, and returns a tuple of [(keyword k) v]"
+  (map-funcs [keyword identity]
+	     (rest (re-find (re-pattern (str "(.+)" d "(.*)")) s))))
