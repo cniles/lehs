c20eb06974316b06229350aa9301a38ee34b1c90
Craig Niles <cniles@alionscience.com>
2014-07-16 11:03:55 -0400

    rename split-at-subseq to split-after-subseq and add new function stream-seq that returns a lazy sequence of a stream

--- src/lehs/common.clj
+++ src/lehs/common.clj
@@ -1,7 +1,10 @@
 (ns lehs.common)
 
-(defn split-at-subseq [s p]
+(defn split-after-subseq [s p]
   (let [c (count p) ps (seq p)]
     (loop [h [] s (lazy-seq s)]
       (cond (or (= ps (take-last c h)) (empty? s)) [h s]
         :else (recur (conj h (first s)) (drop 1 s))))))
+
+(defn stream-seq [s]
+      (repeatedly #(char (.read s))))
