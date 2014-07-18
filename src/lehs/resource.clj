52050a0d5b8e1db66a814ec6f59e56a5edabd69e
Craig Niles <niles.c@gmail.com>
2014-07-18 10:07:31 -0400

    Add extension type mapping
    Adds a map reference for storing file extension to content type
    mappings.  Includes a function for creating them, as well.

--- src/lehs/resource.clj
+++ src/lehs/resource.clj
@@ -11,7 +11,39 @@
 
 ; A reference to the map for all the pages 
 (def pages (ref {}))
+
+; A reference to the map for content types
 (def type-map (ref {}))
+
+(def extension-type-map (ref {}))
+
+(defn get-extension [s]
+  (second (re-find #"\.(.+)$" s)))
+
+(defn get-type [{{{path :path} :uri} :req-ln}]
+  (let [t (get @type-map path :not-found)]
+       (if (= t :not-found)
+	   (extension-type-map (get-extension path))
+	 t)))
+
+(defn map-extensions-to-type [type x & xs]
+  (dosync (alter extension-type-map assoc x type)
+	  (alter extension-type-map into (zipmap xs (repeat type)))))
+
+;
+; Some common extension types
+;
+(defmacro defextmapping 
+  ([t x & xs]
+     (flatten ['map-extensions-to-type t x xs]))
+  ([t x]
+     (list 'map-extensions-to-type t x)))
+
+(defextmapping "text/css" "css")
+(defextmapping "text/html" "htm" "html" "" nil)
+(defextmapping "image/jpeg" "jpeg" "jpg" "jpe")
+(defextmapping "image/gif" "gif")
+(defextmapping "image/png" "png")
 
 (defn add-resource
   "Associates a new page or resource into the map referenced by pages.
@@ -44,17 +76,14 @@
 
 ; Request this resource to kill the server
 (defresource "/killserver"
-  (html [:html [:body [:h1 "killing server"]]])
-  "text/html")
+  (html [:html [:body [:h1 "killing server"]]]))
 
 ; Page not found
 (defresource :404
   (html5 [:html [:body 
     [:h1 "404 - Resource not found"]
-    [:p "The specified resource, " path ", could not be found"]]])
-  "text/html")
+    [:p "The specified resource, " path ", could not be found"]]]))
 
 ; Unsupported operation
 (defresource :500
-  (html5 [:html [:body [:h1 "500 - Unsupported operation: " method]]])
-  "text/html")
+  (html5 [:html [:body [:h1 "500 - Unsupported operation: " method]]]))
