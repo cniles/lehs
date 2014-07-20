68caf855c7c4c15201291fee5286ca27056d1c4e
Craig Niles <niles.c@gmail.com>
2014-07-20 09:42:04 -0400

    refactoring
    foo was moved to a /data directory.  Also refactors bulletin board code
    out.

--- src/lehs/demo/core.clj
+++ src/lehs/demo/core.clj
@@ -3,18 +3,17 @@
         hiccup.page
         lehs.core
         lehs.resource
-        lehs.common
-        lehs.db))
+        lehs.common))
 
-(defresource "/foo.css"
-  (.getBytes (slurp "foo.css")))
+(defresource "/data/foo.css"
+  (.getBytes (slurp "C:data/foo.css")))
 
 (defresource "/air.png"
   (slurp-bytes "air.png"))
 
 (defresource "/"
   (.getBytes (html5 [:html
-          [:head (include-css "foo.css")]
+          [:head (include-css "/data/foo.css")]
           [:body
            [:h1 "Lehs test home"]
            [:p "This website is only a series of pages used to test the lehs webserver."]
@@ -28,43 +27,33 @@
 
 (defresource "/a"
   (.getBytes (html5 [:html
-	[:body
-	[:p "This is page A"]
-	[:ul
-	[:li [:a {:href "b"} "To page B"]]
-	[:li [:a {:href "/"} "Home"]]
-	]]])))
+                     [:head (include-css "/data/foo.css")]
+                     [:body
+                      [:p "This is page A"]
+                      [:ul
+                       [:li [:a {:href "b"} "To page B"]]
+                       [:li [:a {:href "/"} "Home"]]
+                       ]]])))
 
 (defresource "/b"
   (.getBytes (html5 [:html
-	[:body
-	[:p "This is page B"]
-	[:ul
-	[:li [:a {:href "a"} "To page A"]]
-	[:li [:a {:href "/"} "Home"]]
-	]]])))
+                     [:head (include-css "/data/foo.css")]
+                     [:body
+                      [:p "This is page B"]
+                      [:ul
+                       [:li [:a {:href "a"} "To page A"]]
+                       [:li [:a {:href "/"} "Home"]]
+                       ]]])))
 
 (defresource "/c"
   (.getBytes (html5 [:html
-	[:body
-	[:form {:action "d" :method "POST"}
-	"Name: " [:input {:type "text" :name "Name" :value "anon"}] [:br]
-	"Content: " [:input {:type "text" :name "Content" :value ""}] [:br]
-	[:input {:type "submit" :value "Submit"}]
-	]]])))
-
-(defresource "/d"
-  (do
-   (if (= :post method)
-     (do (println (str "adding db entry" message)) (add-bb-entry message)))
-   (let [bb (get-bb)]
-	(.getBytes (html5 [:html
-	      [:body
-	      [:table
-	      (map bb-entry-to-table-row bb)]
-	      [:a {:href "/c"} "Add new message"] [:br]
-	      [:a {:href "/d"} "Refresh"]
-	      ]])))))
+                     [:head (include-css "/data/foo.css")]
+                     [:body
+                      [:form {:action "d" :method "POST"}
+                       "Name: " [:input {:type "text" :name "Name" :value "anon"}] [:br]
+                       "Content: " [:input {:type "text" :name "Content" :value ""}] [:br]
+                       [:input {:type "submit" :value "Submit"}]
+                       ]]])))
 
 (defn -main [& args]
   (run-server 8080))
