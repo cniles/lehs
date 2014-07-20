f829120cde87d2c8c9e3e91795917c8dba3b1688
Craig Niles <niles.c@gmail.com>
2014-07-20 09:30:33 -0400

    add id's to individiual posts
    add the post's database object id into the table row, so that it can be
    referenced with a fragment.  add-bb-entry also generates the objectid
    and returns it.

--- src/lehs/db.clj
+++ src/lehs/db.clj
@@ -1,10 +1,16 @@
 (ns lehs.db
   (:require [monger.core :as mg]
-            [monger.collection :as mc]))
+            [monger.collection :as mc])
+  (:import org.bson.types.ObjectId))
 
 (def conn (mg/connect))
 (def db (mg/get-db conn "mydb"))
-(defn add-bb-entry [entry] (if (every? entry [:Name :Content])
-                             (mc/insert db "bb" (select-keys entry [:Name :Content]))))
+
 (defn get-bb [] (mc/find-maps db "bb"))
-(defn bb-entry-to-table-row [r] [:tr [:td (r :Name ":")] [:td (r :Content)]])
+(defn bb-entry-to-table-row [r] [:tr {:id (r :_id)} [:td (r :Name)] [:td (r :Content)]])
+
+(defn add-bb-entry [entry] (if (every? entry [:Name :Content])
+                             (let [oid (ObjectId.)
+                                   doc (select-keys entry [:Name :Content])]
+                               (mc/insert db "bb" (merge doc {:_id oid}))
+                               oid)))
