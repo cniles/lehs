dde4602bc9f4fc284542b5228c42f37e0f7a810b
Craig Niles <niles.c@gmail.com>
2014-07-20 09:41:24 -0400

    Fix accept-gzip? and gen-post-response
    Gen-post-response wasn't associating with the map returned by the
    resource function (if it returned a map instead of a string).  also
    fixed the regular expression in accept-gzip? to accept content codings
    without a qvalue.

--- src/lehs/response.clj
+++ src/lehs/response.clj
@@ -7,7 +7,7 @@
 
 (defn accept-gzip? [req]
   (if (contains? (req :headers) :Accept-Encoding)
-    (let [es (map #(re-find #"(\*|\w+);q=(0(\.\d?\d?\d?)?|1(\.0?0?0?)?)" %)
+    (let [es (map #(re-find #"(gzip|compress|deflate|identity)(;q=(0(\.\d\d\d)?|1(\.000)?))?" %)
                   (clojure.string/split (-> req :headers :Accept-Encoding) #", "))]
       (contains? (into {} (map (fn [[_ e q]] [(keyword e) q]) es)) :gzip))
     false))
@@ -23,10 +23,11 @@
 (defn gen-get-response [rf req code]
   (let [base-res (gen-base-response req code)
         msg (rf req base-res)]
-    (if (map? msg) (assoc-in msg [:headers :Content-Length] (count (msg :message)))
-        (assoc-in-many base-res
-                       [[[:headers :Content-Length] (count msg)]
-                        [[:message] msg]]))))
+    (if (map? msg)
+      (assoc-in msg [:headers :Content-Length] (count (msg :message)))
+      (assoc-in-many base-res
+                     [[[:headers :Content-Length] (count msg)]
+                      [[:message] msg]]))))
 
 (defn gen-head-response [rf req code]
   (assoc (gen-get-response rf req code) :message ""))
@@ -34,13 +35,14 @@
 (defn gen-post-response [rf req code]
   (let [base-res (gen-base-response req code)
         out (rf req  base-res)]
-    (if (map? out) (assoc-in-many base-res
-                                  [[[:res-ln :code] (if (zero? (count (out :message))) 204 200)]
-                                   [[:headers :Content-Length] (count (out :message))]])
-        (assoc-in-many base-res
-                       [[[:res-ln :code] (if (zero? (count out)) 204 200)]
-                        [[:headers :Content-Length] (count out)]
-                        [[:message] out]]))))                        
+    (if (map? out) 
+      (assoc-in-many out
+                     [[[:res-ln :code] (if (zero? (count (out :message))) 204 200)]
+                      [[:headers :Content-Length] (count (out :message))]])
+      (assoc-in-many base-res
+                     [[[:res-ln :code] (if (zero? (count out)) 204 200)]
+                      [[:headers :Content-Length] (count out)]
+                      [[:message] out]]))))
 
 (def method-fns
   {:get
