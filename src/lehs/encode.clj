15f6d859dcf68d0ae7b3708fce8f5991be893776
Craig Niles <niles.c@gmail.com>
2014-07-22 18:56:33 -0400

    Finish work on encode
    Get base64 encoding working by finishing up all the functions and
    problems.

--- src/lehs/encode.clj
+++ src/lehs/encode.clj
@@ -1,16 +1,24 @@
-(def base64-map (zipmap (range) 
-  (concat (map char (range (int \A) (inc (int \Z))))
-          (map char (range (int \a) (inc (int \z)))))))
-
-(defn get-bit [b i] (if (bit-test b i) 1 0))
-
-(defn get-bits [b] (map #(get-bit b %) (reverse (range 8))))
+(ns lehs.encode
+  (:use lehs.common))
 
 (def pows-of-2 (map #(apply * (repeat % 2)) (range)))
 
-(defn bits-to-int [bs] (reduce + (map #(* % %2) bs (reverse (take (count bs) pows-of-2)))))
+(def base64-map (zipmap (range)
+  (concat (map char (range (int \A) (inc (int \Z))))
+          (map char (range (int \a) (inc (int \z))))
+          (map char (range (int \0) (inc (int \9))))
+          [\+ \/])))
 
-(defn encode-base64 (concat 
-  (partition 4 4 \= 
-    (map #(base64-map (bits-to-int %))
-      (partition 6 6 0 (mapcat get-bits [bs]))))))
+(defn get-bit [b i]
+  (if (bit-test b i) 1 0))
+
+(defn get-bits [b]
+  (map #(get-bit (int b) %) (reverse (range 8))))
+
+(defn bits-to-int [bs] (reduce + (map #(* % %2) bs
+                                      (reverse (take (count bs) pows-of-2)))))
+
+(defn encode-base64 [bs]
+  (flatten (pad-seq 4 \=
+                    (map #(base64-map (bits-to-int %))
+                         (partition 6 6 (repeat 0) (mapcat get-bits bs))))))
